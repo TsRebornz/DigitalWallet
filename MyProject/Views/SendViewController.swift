a898db1344bb2855b06779a95a064a771484308c
Marco Polo <mp@troldev.com>
2016-09-14 14:22:33 +0300

    Touch ID PassCode with problems

--- MyProject/Views/SendViewController.swift
+++ MyProject/Views/SendViewController.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
 import SwiftValidator
+import LocalAuthentication
 
 public class SendViewController : UIViewController, ValidationDelegate, UITextFieldDelegate, ScanViewControllerDelegate {
     @IBOutlet weak var addressTxtField: UITextField!
@@ -48,7 +49,13 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
     let selectorAmountChanged = "sendviewcontroller.amount.changed"
     
     //FlagForSendOperation
-    var allValid : Bool = false
+    var addressValid : Bool = false
+    var amountValid : Bool = false
+    
+    //Authentification
+    
+    var laContext : LAContext!
+    var authenticated = false
     
     //FIXME:Extract to singleton
     var GlobalUserInitiatedQueue: dispatch_queue_t {
@@ -69,6 +76,9 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
         //Need to know when feeData is loaded
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(feeDataChanged), name: selectorFeeChanged, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(amountDataChanged), name: selectorAmountChanged, object: nil)
+        
+        //Authentication
+        self.laContext = LAContext()
         
         //Valiadtion in privateKeyTextField
         validator.registerField(addressTxtField, errorLabel: addressErrorLabel, rules: [RequiredRule(), AddressRule() ])
@@ -198,7 +208,7 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
                     //let amount : Int = Int(textField.text!)!
                     self.changeValidatableFieldToDefault(self.amountTxtField, errorLbl: self.amountErrorLabel)
                     NSNotificationCenter.defaultCenter().postNotificationName(self.selectorAmountChanged, object: self, userInfo: nil)
-                    self.allValid = true
+                    self.amountValid = true
                     
                 } else {
                     // Validation error occurred
@@ -209,6 +219,7 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
                     let errorMessage : String = isAmountNoMoreThanBalance ? "Not number value" : "Amount more than Balance"
                     self.amountErrorLabel.text = errorMessage // works if you added labels
                     self.amountErrorLabel.hidden = false
+                    self.amountValid = false
                 }
             }
         }else {
@@ -227,7 +238,7 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
     
     //MARK:Validation
     public func validationSuccessful(){
-        self.allValid = true
+        self.addressValid = true
         self.changeValidatableFieldToDefault(self.addressTxtField, errorLbl: self.addressErrorLabel!)
         self.transactionProtocol?.changeSendAddress(self.addressTxtField.text!)
     }
@@ -241,6 +252,7 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
                 field!.layer.borderWidth = 1.0
                 error.errorLabel?.text = error.errorMessage // works if you added labels
                 error.errorLabel?.hidden = false
+                self.addressValid = false
             }
         }
     }
@@ -258,6 +270,32 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
         let miners_fee : Int = (self.minersFeeProtocol!.updateMinersFeeWithAmount(Int(strAmount)!))
         self.feeValLbl?.text = "\(miners_fee)"
     }
+    //MARK:
+    
+    //MARK: Authentification
+    
+    func authenticateUser( succes: ( authSucces : Bool ) -> Void ) {
+        var laError : NSError?
+        let reasonString = "Place you finger to verify you identity"
+        var auth = false
+        //Zaglushka
+//        if(authenticated){
+//            //self.laContext.
+//            self.laContext = LAContext()
+//            authenticated = false
+//        }
+        if (laContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthentication, error: &laError)){
+            laContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthentication, localizedReason: reasonString, reply: { (success: Bool, error: NSError?) -> Void in
+                if (success) {
+                    auth = true
+                }
+                succes(authSucces: auth)
+            })
+        } else {
+            succes(authSucces: auth)
+        }
+    }
+    
     //MARK:
     
     //MARK:Actions
@@ -301,8 +339,7 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
     @IBAction func acceptTxBtnTapped(sender: AnyObject) {
         validator.validate(self)
         textFieldShouldReturn(self.amountTxtField)
-        //For HamsterPic
-        //var alertController = UIAlertController(nibName: <#T##String?#>, bundle: <#T##NSBundle?#>)
+        var txsend = false
         var title = "Send Transaction Error"
         var message = "Error not all properties are valid"
         var uiAlertActions : [UIAlertAction] = [UIAlertAction]()
@@ -312,19 +349,26 @@ public class SendViewController : UIViewController, ValidationDelegate, UITextFi
             })
         uiAlertActions.append(cancelAction)
         
-        if (allValid){
+        if (addressValid && amountValid){
             title = "Send Transaction"
             message = "You want to send \(self.amountTxtField!.text!) satoshi. To address \(self.addressTxtField!.text!). With miners fee \(self.feeValLbl!.text!)"
             let okAction = UIAlertAction(title: "Send", style: UIAlertActionStyle.Default, handler: { UIAlertAction in
-                //Progress bar
-                self.transactionProtocol?.createTransaction()
-                self.transactionProtocol?.signTransaction()
-                self.transactionProtocol?.sendTransaction({ response in
-                   
-                    //Send data about Transaction to viewControllers
-                    //Throw back to previus screen or transtion screen and save sended transaction to coreData
-                    
-                    self.dismissViewControllerAnimated(true, completion: nil)
+                //FIXME: Progress bar
+                self.authenticateUser({ auth in
+                    if auth && !txsend {
+                        dispatch_sync(dispatch_get_main_queue(),{
+                            txsend = true
+                        })                        
+                        self.transactionProtocol?.createTransaction()
+                        self.transactionProtocol?.signTransaction()
+                        self.transactionProtocol?.sendTransaction({ response in
+                            //Send data about Transaction to viewControllers
+                            //Throw back to previus screen or transtion screen and save sended transaction to coreData
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        })
+                    }else{
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    }                                        
                 })
             })
             uiAlertActions.append(okAction)
