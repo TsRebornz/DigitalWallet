ced25e00d509819fefeccf13e64df765a7371d36
Marco Polo <mp@troldev.com>
2016-09-14 21:08:59 +0300

    Setting view and btn

---
+++ MyProject/Views/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  MyProject
+//
+//  Created by username on 14/09/16.
+//  Copyright © 2016 BCA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class SettingsViewController : UITableViewController {
+    public override func viewDidLoad() {
+        
+    }
+    
+    
+    @IBAction func cancel(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
